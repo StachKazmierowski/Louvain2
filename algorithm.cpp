e16bed470e573423452b6d0c5a6d98498f858add
Stach <staho.kazmierowski@gmail.com>
2020-05-09 18:04:21 +0200

    more datas

---
+++ algorithm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stach on 09.05.2020.
+//
+
+#include "algorithm.h"
---
+++ algorithm.h
@@ -0,0 +1,8 @@
+//
+// Created by stach on 09.05.2020.
+//
+
+#ifndef LOUVAIN_ALGORITHM_H
+#define LOUVAIN_ALGORITHM_H
+
+#endif //LOUVAIN_ALGORITHM_H
