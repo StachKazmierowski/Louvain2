2ccb53fe434dc96b6c5617f6627f363b28d63b99
Stach <staho.kazmierowski@gmail.com>
2020-05-09 12:36:55 +0200

    almost ready to implement phases

--- louvain.c
+++ louvain.c
@@ -6,8 +6,8 @@
 
 float getKi(Graph* g, int vertice){
     float sum = 0.0;
-    int endIndex = EDGES_IDX(g, vertice);
-    for(int i = EDGES_IDX(g, vertice - 1); i <  endIndex; i++){
+    for(int i = EDGES_IDX(g,vertice-1); i < EDGES_IDX(g,vertice); i++){
+//        printf("%d\n", i);
         sum += g->edges[i].weight;
     }
     return sum;
@@ -15,8 +15,7 @@ float getKi(Graph* g, int vertice){
 
 float getKiin(Graph *g, int vertice, int* cliques, int clique){
     float sum = 0.0;
-    int endIndex = EDGES_IDX(g, vertice);
-    for(int i = 0; i < endIndex; i++){
+    for(int i = EDGES_IDX(g,vertice-1); i < EDGES_IDX(g,vertice); i++){
         Edge e = g->edges[i];
         if(cliques[e.to] == clique){
             sum += e.weight;
@@ -36,15 +35,38 @@ float sigmaIN(Graph* g, int* cliques, int clique){
     return sum / 2;
 }
 
-float sigmaTOT(Graph* g, int* cliques, int clique, int vertice){
+float sigmaTOT(Graph* g, int* cliques, int clique){
     float sum = 0.0;
     for(int i = 0; i < g->numEdges; i++){
         Edge e = g->edges[i];
-        if(e.from == vertice && cliques[e.to] == clique){
+        if(cliques[e.to] == clique || cliques[e.from] == clique){
             sum += e.weight;
         }
     }
-    return sum;
+    return sum / 2;
+}
+
+void recalcSigmaTOTS(Graph*g, float* sigmaTot, int* cliques){
+    for(int i = 0; i < g->size; i++){
+        sigmaTot[i] = 0.0;
+    }
+    for(int i =0 ; i < g->numEdges; i++){
+        Edge e = g->edges[i];
+        int cliqueFrom = cliques[e.from];
+        int cliqueTo = cliques[e.to];
+        if(cliqueFrom == cliqueTo){
+            sigmaTot[cliqueFrom] += e.weight / 2;
+        } else {
+            sigmaTot[cliqueFrom] += e.weight / 2;
+            sigmaTot[cliqueTo] += e.weight / 2;
+        }
+    }
+}
+
+void printCliques(Graph* g, int* cliques){
+    for(int i = 0; i < g->size; i++){
+        printf("%d %d\n", i, cliques[i]);
+    }
 }
 
 float weightsSum(Graph* g){
@@ -56,6 +78,39 @@ float weightsSum(Graph* g){
     return sum / 2;
 }
 
+float deltaQ(Graph* g, int* cliques, int clique, int vertice){
+    float kiin = getKiin(g, vertice, cliques, clique);
+    float ki = getKi(g, vertice);
+    float sigmaT = sigmaTOT(g, cliques, clique);
+    float m = weightsSum(g);
+    printf("%f %f %f %f \n", kiin, ki, sigmaT, m);
+    return ( kiin - ((ki * sigmaT) / (2 * m))) / m ;
+}
+
+int bestClique(Graph* g, int* cliques, int vertice, float* sigmaTOTs, float m){
+    float bestDelta = 0.0;
+    int bestClique = -1;
+    for(int  i = EDGES_IDX(g, vertice - 1); i < EDGES_IDX(g, vertice); i++){
+        Edge edge = g->edges[i];
+        int to = edge.to;
+        int consideredClique = cliques[to];
+        if(consideredClique != cliques[vertice] && consideredClique != bestClique){
+            float delta = deltaQ(g, cliques, consideredClique, vertice);
+            if(delta > bestDelta){
+                bestClique = consideredClique;
+                bestDelta = delta;
+            } else if (delta == bestClique && bestClique > consideredClique){
+                bestClique = consideredClique;
+            }
+        }
+    }
+    return bestClique;
+}
+
+//int canMove(int currentClique, int targetClique, int* cliqueSizes){
+//    if(cu)
+//}
+
 int sameClique(int* cliques, int vertice1, int vertice2){
     if(cliques[vertice1] == cliques[vertice2]){
         return 1;
@@ -64,25 +119,38 @@ int sameClique(int* cliques, int vertice1, int vertice2){
     }
 }
 
-float Q(Graph* g, int* cliques){
-    float sum = 0.0;
-    float m_twice = 2 * weightsSum(g);
-    for(int i = 0; i < g->numEdges; i++){
-        Edge e = g->edges[i];
-        int from = e.from;
-        int to = e.to;
-        if(sameClique(cliques, from, to) == 1){
-            sum += e.weight - (getKi(g, from) * getKi(g, to) /  m_twice);
+float edgeWeight(Graph* g, int verticeFrom, int verticeTo){
+    for(int  i = EDGES_IDX(g, verticeFrom - 1); i < EDGES_IDX(g, verticeFrom); i++){
+        if(g->edges[i].to == verticeTo){
+            return g->edges[i].weight;
         }
     }
-    return sum / m_twice;
+    return 0.0;
 }
 
-float deltaQ(Graph* g, int* cliques, int clique, int vertice){
-    float kiin = getKiin(g, vertice, cliques, clique);
-    float ki = getKi(g, vertice);
-    float sigmaT = sigmaTOT(g, cliques, clique, vertice);
-    float m = weightsSum(g);
-//    printf("%f %f %f %f \n", kiin, ki, sigmaT, m);
-    return (kiin - ki * sigmaT) / m;
+float Q(Graph* g, int* cliques, float m){
+    float sum = 0.0;
+    for(int i = 0 ; i < g->size; i++){
+        for(int j = 0; j < g->size; j++){
+            if(sameClique(cliques, i, j) == 1){
+                float weight = edgeWeight(g, i, j);
+//                printf("%f\n", weight);
+                sum += weight - (getKi(g, i) * getKi(g, j) / (2 * m));
+            }
+        }
+    }
+    return sum / (2 * m);
 }
+
+void changeEdges(Graph* g, const int* cliques, const int* minVerticeInClique){
+    for(int i =0 ; i < g->numEdges; i++){
+        Edge* edge = g->edges + i;
+        int from = edge->from;
+        int to = edge->to;
+        int superVerticeFrom = minVerticeInClique[cliques[from]];
+        int superVerticeTo = minVerticeInClique[cliques[to]];
+        edge->from = superVerticeFrom;
+        edge->to = superVerticeTo;
+    }
+}
+
